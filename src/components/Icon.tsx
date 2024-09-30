a060b4607e6f5921831ff8fa3ab80b3d1db75033
kimgoat <kimgoatlove@gmail.com>
2024-09-30 13:19:19 +0900

    feat: Icon 컴포넌트 구현

---
+++ src/components/Icon.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { ReactComponent as LocationIcon } from "@assets/svg/icons/location.svg";
+import { ReactComponent as TravelIcon } from "@assets/svg/icons/travel.svg";
+import { ReactComponent as BuildingIcon } from "@assets/svg/icons/building.svg";
+import { ReactComponent as LocationRecommandIcon } from "@assets/svg/icons/location-recommand.svg";
+import styled from "styled-components";
+
+export type IconName =
+  | "location"
+  | "travel"
+  | "building"
+  | "location_recommand";
+
+interface IconProps {
+  name: IconName;
+  color?: string;
+  hoverColor?: string; // hover 시 적용할 색상
+  size?: number;
+  strokeWidth?: number;
+}
+
+const Icon: React.FC<IconProps & { className?: string }> = ({
+  name,
+  color = "currentColor",
+  hoverColor = "currentColor",
+  size = 24,
+  strokeWidth = 1,
+  className,
+}) => {
+  const icons: { [key in IconName]: React.FC<React.SVGProps<SVGSVGElement>> } =
+    {
+      location: LocationIcon,
+      travel: TravelIcon,
+      building: BuildingIcon,
+      location_recommand: LocationRecommandIcon,
+    };
+
+  const IconComponent = icons[name];
+
+  return (
+    <Container className={className}>
+      <StyledIcon
+        as={IconComponent}
+        width={size}
+        height={size}
+        $color={color}
+        $hoverColor={hoverColor}
+        $strokeWidth={strokeWidth}
+      />
+    </Container>
+  );
+};
+
+export default Icon;
+
+const Container = styled.div``;
+
+const StyledIcon = styled.svg<{
+  $color: string;
+  $hoverColor: string;
+  $strokeWidth: number;
+}>`
+  stroke: ${(props) => props.$color};
+  stroke-width: ${(props) => props.$strokeWidth};
+
+  * {
+    stroke: ${(props) => props.$color};
+    stroke-width: ${(props) => props.$strokeWidth};
+  }
+
+  &:hover {
+    stroke: ${(props) => props.$hoverColor}; // hover 시 색상 변경
+    * {
+      stroke: ${(props) => props.$hoverColor}; // 모든 내부 요소에 적용
+    }
+  }
+`;
