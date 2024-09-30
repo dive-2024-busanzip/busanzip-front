a3aab941c547bc3d13a6c09df4782d3e192d9b91
kimgoat <kimgoatlove@gmail.com>
2024-09-30 20:12:08 +0900

    chore: Map 페이지에 Accordion 컴포넌트 적용

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,9 +1,29 @@
+import Accordion from "@components/common/Accordion";
+import { ROUTES } from "@constants/routes";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 export const Map = () => {
-  return <Wrapper></Wrapper>;
+  const location = useLocation();
+  return (
+    <Wrapper>
+      {location.pathname === ROUTES.STAY && (
+        <Accordion title={"정착"} content={"정착 데이터"} />
+      )}
+      {location.pathname === ROUTES.TEAVEL && (
+        <Accordion title={"관광"} content={"관광 데이터"} />
+      )}
+      {location.pathname === ROUTES.AROUND && (
+        <Accordion title={"내 주변"} content={"내 주변 데이터"} />
+      )}
+    </Wrapper>
+  );
 };
 
 export default Map;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+`;
