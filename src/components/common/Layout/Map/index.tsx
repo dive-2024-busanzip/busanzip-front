37b6b86c85d3dcbfc72e36e6c00ab0157ccf04fe
kimgoat <kimgoatlove@gmail.com>
2024-09-30 15:35:00 +0900

    feat: Map 페이지 레이아웃 구현

---
+++ src/components/common/Layout/Map/index.tsx
@@ -0,0 +1,14 @@
+import Navigation from "@components/common/Navigation";
+import styled from "styled-components";
+
+export const MapLayout = () => {
+  return (
+    <Wrapper>
+      <Navigation />
+    </Wrapper>
+  );
+};
+
+export default MapLayout;
+
+const Wrapper = styled.div``;
