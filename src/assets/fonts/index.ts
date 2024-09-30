b353eadb4402be57a971c1ae5000963a15351ef7
kimgoat <kimgoatlove@gmail.com>
2024-09-30 12:21:08 +0900

    chore: 폰트 파일 경로 설정

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,13 @@
+export const FONT_PATHS = {
+  PRETENDARD_BOLD_WOFF2: require("./Pretendard/woff2/Pretendard-Bold.woff2"),
+  PRETENDARD_BOLD_WOFF: require("./Pretendard/woff/Pretendard-Bold.woff"),
+  PRETENDARD_BOLD_TTF: require("./Pretendard/ttf/Pretendard-Bold.ttf"),
+
+  PRETENDARD_REGULAR_WOFF2: require("./Pretendard/woff2/Pretendard-Regular.woff2"),
+  PRETENDARD_REGULAR_WOFF: require("./Pretendard/woff/Pretendard-Regular.woff"),
+  PRETENDARD_REGULAR_TTF: require("./Pretendard/ttf/Pretendard-Regular.ttf"),
+
+  PRETENDARD_THIN_WOFF2: require("./Pretendard/woff2/Pretendard-Thin.woff2"),
+  PRETENDARD_THIN_WOFF: require("./Pretendard/woff/Pretendard-Thin.woff"),
+  PRETENDARD_THIN_TTF: require("./Pretendard/ttf/Pretendard-Thin.ttf"),
+};
